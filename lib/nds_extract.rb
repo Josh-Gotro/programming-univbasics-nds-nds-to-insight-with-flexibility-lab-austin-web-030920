fb9accb117bb0afec9eaa10641872bd0da8bb8e8
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-21 18:20:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -65,8 +65,9 @@ def movies_with_director_key(name, movies_collection)
 end
 
 def gross_per_studio(collection)
-    puts "****************LOL****************"
-    puts collection.inspect
+    # puts "****************LOL****************"
+    # puts collection.inspect
+    collection.pp
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from
   # each studio.
